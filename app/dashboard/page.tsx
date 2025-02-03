5bd00574f6b17583e222494bafec333fb9bcc37e
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2025-02-04 01:45:35 +0200

    d1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
 export default function DashboardPage() {
-  return <h1>Welcome to Dashboardpage!</h1>;
+	return <h1>Welcome to Dashboardpage!</h1>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,13 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
+const inter = localFont({
+	src: './fonts/Inter.ttf',
+	variable: '--font-inter',
 })
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
+const interItalic = localFont({
+	src: './fonts/Inter-Italic.ttf',
+	variable: '--font-inter-italic',
 })
 
 export const metadata: Metadata = {
@@ -25,11 +23,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${geistSans.className} ${geistMono.className} antialiased`}
-			>
-				{children}
-			</body>
+			<body className={`${inter.className} antialiased`}>{children}</body>
 		</html>
 	)
 }
