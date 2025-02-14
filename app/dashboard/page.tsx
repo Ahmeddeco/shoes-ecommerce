54d7a76063efd1bf5038e85c4ef4a9051c818ab9
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2025-02-14 21:04:32 +0200

    add DashboardCard and RecentSalesAvatar components; update dashboard layout and data structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,49 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { cardContent } from '@/constants/cardContent'
+import DashboardCard from '@/components/layout/dashboard/DashboardCard'
+import RecentSalesAvatar from '@/components/layout/dashboard/RecentSalesAvatar'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { RecentSalesAvatarData } from '@/constants/RecentSalesAvatarData'
 
 export default function DashboardPage() {
 	return (
 		<>
-			<div className='grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4'>
-				{cardContent.map(({ CardIcon, cardTitle, iconColor, id, p1, p2 }) => (
-					<Card key={id}>
-						<CardHeader className='flex flex-row items-center justify-between pb-2'>
-							<CardTitle className='capitalize'>{cardTitle}</CardTitle>
-							<CardIcon className={`h-4 w-4 ${iconColor}`} />
-						</CardHeader>
-						<CardContent>
-							<p className='text-2xl font-bold capitalize'>{p1}</p>
-							<p className='text-xs text-muted-foreground capitalize'>{p2}</p>
-						</CardContent>
-					</Card>
-				))}
-			</div>
+			<DashboardCard />
+			<section className='grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3 mt-10'>
+				<Card className='xl:col-span-2'>
+					<CardHeader>
+						<CardTitle>Transactions</CardTitle>
+						<CardDescription>
+							Recent transactions from your store
+						</CardDescription>
+					</CardHeader>
+				</Card>
+				<Card className=''>
+					<CardHeader>
+						<CardTitle>Recent Sales</CardTitle>
+					</CardHeader>
+					<CardContent className='flex flex-col gap-8'>
+						{RecentSalesAvatarData.map(
+							({ avatarImg, email, id, name, price }) => (
+								<div className='flex items-center gap-4' key={id}>
+									<RecentSalesAvatar
+										id={id}
+										name={name}
+										avatarImg={avatarImg}
+										email={email}
+										price={price}
+									/>
+								</div>
+							)
+						)}
+					</CardContent>
+				</Card>
+			</section>
 		</>
 	)
 }
---
+++ constants/RecentSalesAvatarData.ts
@@ -0,0 +1,38 @@
+export type RecentSalesAvatarDataType = {
+  id: number,
+  name: string,
+  avatarImg: string,
+  email: string,
+  price: number
+}
+
+export const RecentSalesAvatarData: RecentSalesAvatarDataType[] = [
+  {
+    id: 1,
+    name: 'ahmed mohamed',
+    avatarImg: '',
+    email: 'ahmedmohamed@email.com',
+    price: 23
+  },
+  {
+    id: 2,
+    name: 'abbas mostafa',
+    avatarImg: '',
+    email: 'abbas_mostafa@email.com',
+    price: 45
+  },
+  {
+    id: 3,
+    name: 'moataz ezzat',
+    avatarImg: '',
+    email: 'moataz-ezzat@email.com',
+    price: 17
+  },
+  {
+    id: 4,
+    name: 'hoda ahmed',
+    avatarImg: '',
+    email: 'hoda_ahmed@email.com',
+    price: 85
+  },
+]
