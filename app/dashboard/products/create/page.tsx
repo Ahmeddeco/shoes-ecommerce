b1585174143e37275b068def7bb3f68e5f93d679
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2025-03-10 20:01:48 +0200

    add new UI components for product creation; implement textarea, input, label, and switch components; enhance dashboard navigation with active link styling; update tailwind configuration

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,3 +1,92 @@
+import { Button } from '@/components/ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
+import { Switch } from '@/components/ui/switch'
+import { Textarea } from '@/components/ui/textarea'
+import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
+
 export default function CreatePage() {
-  return <h1>Welcome to Createpage!</h1>;
+	return (
+		<form>
+			<div className='flex items-center gap-4'>
+				<Button variant={'outline'} asChild>
+					<Link href={'/dashboard/products'}>
+						<ChevronLeft className='size-4' />
+					</Link>
+				</Button>
+				<h1 className='capitalize text-xl font-semibold tracking-tight'>
+					new product
+				</h1>
+			</div>
+			<Card className='mt-5'>
+				<CardHeader>
+					<CardTitle>Product Details</CardTitle>
+					<CardDescription>
+						In this form you can create yoiur product
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<div className='flex flex-col gap-6'>
+						<div className='flex flex-col gap-3'>
+							<Label>Name</Label>
+							<Input
+								type='text'
+								placeholder='Product Name'
+								className='w-full'
+							/>
+						</div>
+						<div className='flex flex-col gap-3'>
+							<Label>Description</Label>
+							<Textarea
+								placeholder='Write your description here ...'
+								className='w-full'
+							/>
+						</div>
+						<div className='flex flex-col gap-3'>
+							<Label>Price</Label>
+							<Input type='number' placeholder='$55' className='w-full' />
+						</div>
+						<div className='flex flex-col gap-3'>
+							<Label>Featured Product</Label>
+							<Switch />
+						</div>
+						<div className='flex flex-col gap-3'>
+							<Label>Status</Label>
+							<Select>
+								<SelectTrigger>
+									<SelectValue placeholder='Select Status' />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectItem value='draft' className='capitalize'>
+										draft
+									</SelectItem>
+									<SelectItem value='published' className='capitalize'>
+										published
+									</SelectItem>
+									<SelectItem value='archived' className='capitalize'>
+										archived
+									</SelectItem>
+								</SelectContent>
+							</Select>
+						</div>
+					</div>
+				</CardContent>
+			</Card>
+		</form>
+	)
 }
--- components/layout/admin/DashboardNavigation.tsx
+++ components/layout/admin/DashboardNavigation.tsx
@@ -1,12 +1,24 @@
+'use client'
+
 import { links } from '@/constants/links'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const DashboardNavigation = () => {
+	const pathName = usePathname()
+
 	return (
 		<>
 			{links.map(({ href, name }, i) => (
-				<Link href={href} key={i}>
+				<Link
+					href={href}
+					key={i}
+					className={`${
+						pathName === href
+							? 'text-foreground'
+							: 'text-muted-foreground hover:text-foreground transition-all'
+					} capitalize`}
+				>
 					{name}
 				</Link>
 			))}
